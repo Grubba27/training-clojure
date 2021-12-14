eea8ef68e3985c5efbe6e3d09191f25c552d6bdb
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2021-12-14 19:03:00 -0300

    Started TDD

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -25,4 +25,26 @@
         (* salary mid-imposts)
         (* salary high-imposts)))))
 
+
+;; started tdd
+;; Fully tested function
+(defn delivery-tax
+  "Calculate the price of the delivery tax based on input based on this table:
+   80 => $ -> 15
+   80  < $ > 250 -> 5
+   $ >=  250  -> 0"
+  [final-items-value]
+  (let [free-tax 0
+        mid-tax 5
+        max-tax 15
+        min-price-limit 80
+        max-price-limit 250
+        ]
+    (cond
+      (<= final-items-value 0) "err"
+      (<= final-items-value min-price-limit) max-tax
+      (and (> final-items-value min-price-limit) (< final-items-value max-price-limit)) mid-tax
+      (>= final-items-value max-price-limit) free-tax
+      )))
+
 (defn -main [] (println (salary-imposts 1900)))
--- test/hello/core_test.clj
+++ test/hello/core_test.clj
@@ -2,6 +2,22 @@
   (:require [clojure.test :refer :all]
             [hello.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+
+(deftest tax-below-80-test
+  (testing "Given an input of 80 it should return 15"
+    (is (= 15 (delivery-tax 1)))
+    (is (= 15 (delivery-tax 80)))))
+
+
+(deftest tax-between-80-and-250-test
+  (testing "Given an input of 85 0r 249 it should return 5"
+    (is (= 5 (delivery-tax 85)))
+    (is (= 5 (delivery-tax 249)))))
+
+
+(deftest tax-bigger-than-250-test
+  (testing "Given an input bigger than 250 it should return 0"
+    (is (= 0 (delivery-tax 250)))
+    (is (= 0 (delivery-tax 300)))))
